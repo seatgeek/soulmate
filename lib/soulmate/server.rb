8f8ab3070ab65dad618a0da4641e51b1bf6c5a87
Eric Waller <erwaller@gmail.com>
2011-05-10 11:13:23 -0400

    use Rack::JSONP to provide jsonp support

--- lib/soulmate/server.rb
+++ lib/soulmate/server.rb
@@ -1,10 +1,13 @@
 require 'sinatra/base'
 require 'soulmate'
+require 'rack/contrib'
 
 module Soulmate
 
   class Server < Sinatra::Base
     include Helpers
+    
+    use Rack::JSONP
     
     before do
       content_type 'application/json', :charset => 'utf-8'
