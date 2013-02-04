eca7ca17a7ed6538f9e16d6da57009d076f91739
Eric Waller <erwaller@gmail.com>
2013-02-04 12:17:47 -0500

    unicode support deserves v1.0.0

--- lib/soulmate/version.rb
+++ lib/soulmate/version.rb
@@ -1,8 +1,8 @@
 module Soulmate
   module Version
-    MAJOR = 0
-    MINOR = 1
-    PATCH = 3
+    MAJOR = 1
+    MINOR = 0
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
